22474687ce143ad1ecdf11cb993cd0bf28f09a2e
mvcloud <114954862+mvcloud@users.noreply.github.com>
2023-04-27 11:55:01 -0400

    Delete src directory

--- src/MergeSort.h
+++
@@ -1,7 +0,0 @@
-#include "recipe.h"
-
-// Merge Sort functions
-
-void mergeSort(vector<Recipe*>& recipes, int left, int right);
-
-void merge(vector<Recipe*>& recipes, int left, int mid, int right);
--- src/WordsOfEncouragement.cpp
+++
@@ -1,7 +0,0 @@
-#include "WordsOfEncouragement.h"
-
-string Encouragement::RandomEncouragement() {
-    std::mt19937 random(time(0));
-    uniform_int_distribution<> distribution(0, 10);
-    return arr[distribution(random)];
-}
