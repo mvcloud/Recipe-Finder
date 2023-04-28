cc0b008d6bb9bfe23e42d8e77705d57a5941a577
mvcloud <114954862+mvcloud@users.noreply.github.com>
2023-04-27 22:07:07 -0400

    Update WordsOfEncouragement.cpp

--- src/WordsOfEncouragement.cpp
+++ src/WordsOfEncouragement.cpp
@@ -1,5 +1,6 @@
 #include "WordsOfEncouragement.h"
 
+// returns a random string from the array
 string Encouragement::RandomEncouragement() {
     std::mt19937 random(time(0));
     uniform_int_distribution<> distribution(0, 10);
